8de47e309bfb9e938077e291cbd49df4133fc3df
xin <liuxin@163.com>
2020-12-23 22:22:16 +0800

    springcloud-config配置中心

---
+++ cloud-config-client-3355/src/main/java/com/feiniu/springcloud/controller/configController.java
@@ -0,0 +1,26 @@
+package com.feiniu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liuxin
+ * @Date 2020/12/23 22:07
+ * Copyright (C) 1997-2020 康成投资（中国）有限公司
+ * <p>
+ * http://www.rt-mart.com
+ * <p>
+ * 版权归本公司所有，不得私自使用、拷贝、修改、删除，否则视为侵权
+ */
+@RestController
+public class configController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
